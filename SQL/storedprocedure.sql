08b2092edeb417676263142b4ee41f0e31139c8b
sowjanyaSadashiva <67077357+SowjanyaSadashivu@users.noreply.github.com>
2023-10-17 14:21:36 -0500

    created stored procedure

---
+++ SQL/storedprocedure.sql
@@ -0,0 +1,40 @@
+create database SQLPractice;
+
+use sqlpractice;
+
+create table userdetails(
+	id int auto_increment primary key,
+    uname varchar(25),
+    age int,
+    location varchar(25)
+);
+
+DELIMITER $$
+
+create procedure insertuserdetails()
+begin
+	declare counter int default 1;
+	
+    while counter <=100 do
+		set @uname = concat('abc', counter);
+		set @age = counter;
+		set @location = concat('xyz', counter);
+    
+		insert into userdetails (uname, age, location) values(@uname, @age, @location);
+        
+		set counter = counter + 1;
+		if (counter % 10 = 0) then
+			select concat(counter, 'rows inserted') as message;
+		end if;
+	end while;
+end $$
+
+delimiter ;
+		
+call insertuserdetails();
+
+select * from userdetails;
+
+truncate userdetails;
+
+
